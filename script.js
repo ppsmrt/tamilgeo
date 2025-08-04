bff0cc0ca5c60a528e63ee8c0d4ee513f4f2791c
ppsmrt <124156458+ppsmrt@users.noreply.github.com>
2025-08-05 00:43:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function renderCustomPosts() {
+  const customPosts = JSON.parse(localStorage.getItem("customPosts") || "[]");
+  customPosts.forEach(post => {
+    const div = document.createElement("div");
+    div.className = "post";
+    div.innerHTML = `
+      <h2>${post.title}</h2>
+      <p>${post.content.substring(0, 100)}...</p>
+      <p><small>By ${post.author} on ${post.date}</small></p>
+    `;
+    postContainer.appendChild(div);
+  });
+}
+
 const postContainer = document.getElementById("posts");
 
 fetch("https://public-api.wordpress.com/rest/v1.1/sites/tamilgeo.wordpress.com/posts/")
